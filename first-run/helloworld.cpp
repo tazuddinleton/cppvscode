794125a5978d6bb72582ff374a0ee197a0fd13a0
Taz Uddin <getleton@gmail.com>
2020-06-29 23:41:48 +0600

    First commit

---
+++ first-run/helloworld.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+#include <string>
+int main(){
+
+    std::cout << "Enter your name" << std::endl;
+
+    std::string name;
+
+    std::cin >> name;
+
+    std::cout << "Hello, " << name << std::endl;
+
+    return 0;
+}
